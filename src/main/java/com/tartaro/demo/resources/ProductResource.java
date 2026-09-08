14d4e5da9456189b4f82b3cfb810f8abf9012d40
Caio Gustavo <caiogggustavo49@gmail.com>
2026-09-08 20:47:38 -0300

    feat: ProductResources created

---
+++ src/main/java/com/tartaro/demo/resources/ProductResource.java
@@ -0,0 +1,47 @@
+package com.tartaro.demo.resources;
+
+import com.tartaro.demo.entities.Product;
+import com.tartaro.demo.services.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+
+    private final ProductService productService;
+
+    public ProductResource(ProductService productService) {
+        this.productService = productService;
+    }
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll() {
+        List<Product> products = productService.findAll();
+        return ResponseEntity.ok().body(products);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Product> findById(@PathVariable(value = "id") Long id) {
+        Product product = productService.findById(id);
+        return ResponseEntity.ok().body(product);
+    }
+    @PostMapping
+    public ResponseEntity<Product> insert(@RequestBody Product product) {
+        Product newProduct = productService.insert(product);
+        URI uri =  ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newProduct.getId()).toUri();
+        return ResponseEntity.created(uri).body(newProduct);
+    }
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Product> delete(@PathVariable(value = "id") Long id) {
+         productService.delete(id);
+         return ResponseEntity.noContent().build();
+    }
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Product> update(@PathVariable(value = "id") Long id, @RequestBody Product product) {
+        Product p = productService.update(product, id);
+        return ResponseEntity.ok().body(p);
+    }
+}
